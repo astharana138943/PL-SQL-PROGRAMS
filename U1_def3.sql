27ea940167df86f81d23eee42013e354b4c50793
astharana138943 <astha.rana138943@marwadiuniversity.ac.in>
2026-02-13 08:35:34 +0530

    Add files via upload

---
+++ U1_def3.sql
@@ -0,0 +1,16 @@
+--calculate discount on product
+
+set serveroutput on
+
+declare
+	pname char(10):='&pname';
+	pqty number(10):=&pqty;
+	pprice number(10):=&pprice;	
+	total number(10);
+	discount number(10);
+begin 
+	total:=pprice*pqty;
+	discount:=total*10/100;
+	dbms_output.put_line('discount on this 	product is='||discount);
+end;
+/
